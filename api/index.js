b70dc74d05a31af2ef44ae00154f7b449badbcac
Mauro Colucci <colucci.mauro@gmail.com>
2022-11-27 19:40:39 -0300

    post and share functionality using react query

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv'
 dotenv.config({path: './config/.env'})
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
+import multer from 'multer'
 import authRoutes from './routes/auth.js'
 import userRoutes from './routes/users.js'
 import postRoutes from './routes/posts.js'
@@ -22,6 +23,22 @@ app.use(cors({
     origin: "http://localhost:3000"
 }))
 app.use(cookieParser())
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../client/public/upload')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+})
+  
+const upload = multer({ storage: storage })
+
+app.post("/api/upload", upload.single('file'), (req, res)=>{
+    const file = req.file
+    res.status(200).json(file.filename)
+})
 
 app.use('/api/auth', authRoutes)
 app.use('/api/users', userRoutes)
